e570e088fd0bbdefe503653767254ba98d414431
Shannon Coward <shannon.armon@gmail.com>
2016-06-07 20:57:57 -0400

    link fixes
    fixed links

--- webRequestTest/ViewController.swift
+++ webRequestTest/ViewController.swift
@@ -25,13 +25,34 @@ class ViewController: UIViewController {
     override func viewDidAppear(animated: Bool) {
         let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
         webView.frame = frame
+        
+        loadRequest("https://developer.apple.com/swift/blog/")
+        
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func loadRequest(urlStr: String) {
+        
+        let url = NSURL(string: urlStr)!
+        let request = NSURLRequest(URL: url)
+        webView.loadRequest(request)
+    }
 
+    @IBAction func celebrityGossipButtonPressed(sender: AnyObject) {
+        loadRequest("http://www.tmz.com/")
+    }
+    
+    @IBAction func swiftButtonPressed(sender: AnyObject) {
+        loadRequest("https://developer.apple.com/swift/blog/")
+    }
+    
+    @IBAction func steak(sender: AnyObject) {
+        loadRequest("http://steaks.com")
+    }
 }
 
